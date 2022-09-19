fc5301591315b1a05c2ed8a79b66183479171db8
spoukke <sacha.bernheim@hey.com>
2022-09-19 15:31:15 +0200

    feat: add solution

---
+++ solution/deploy.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -e
+
+# Helpers for readability.
+bold=$(tput bold)
+normal=$(tput sgr0)
+function _info() {
+    echo "${bold}${1}${normal}"
+}
+
+CLUSTER_NAME=padok-training
+
+# Run script from directory where the script is stored.
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+_info "🔧 Building guestbook dokcer image..."
+docker build -t guestbook:v0.1.0 -f Dockerfile ../
+
+_info "📥 Loading guestbook image to kind cluster..."
+kind load docker-image --name=$CLUSTER_NAME guestbook:v0.1.0
+
+_info "📥 Deploying redis..."
+helm upgrade --install redis redis \
+             --repo https://charts.bitnami.com/bitnami \
+             --set architecture=standalone \
+             --set auth.enabled=false
+
+_info "📥 Deploying guestbook..."
+kubectl apply -f manifests/
+
+_info "🚀 You are ready to go!"
